06b3e9c1d7a9461987188b8d70163e8c31ea9a2a
hthoeglund <77021425+hthoeglund@users.noreply.github.com>
2021-02-04 13:16:37 -0600

    Update Main.java

--- challenge2/Main.java
+++ challenge2/Main.java
@@ -5,6 +5,7 @@ public class Main{
         System.out.println(addN(4));
     }
 
+    //Adds n to every integer in an inputted string
     public static String addN(int n){
         Scanner console = new Scanner(System.in);
         System.out.print("What string would you like to use? ");
